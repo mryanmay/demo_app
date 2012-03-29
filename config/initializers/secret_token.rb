65d7c9744b750c4d125627bcae9ef6e20f373149
rmay <rmay@middil.com>
2012-03-29 16:28:40 -0600

    mysql

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DemoApp::Application.config.secret_token = '73f84739dd4f915f353d67b4f01c0ed046a5f85cd4b450b7b97ab1ecb4e647c56b25782f9da7294aac0846b8c9cdd1e6c05db24663972f34ec34de063b4575c8'
+DemoApp::Application.config.secret_token = 'c17a9e66b4dbb8932151074a63ad7d94f24fc7eb100a6cd615a8245466fdbca388ed200e15b6dc8fea528786db8ebdb7b0c775978caca3623cebc3db82a1cb0e'
